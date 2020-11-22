12d134694206911af398d58bde8013ca64d2f27e
JB <shin0343@mail.hongik.ac.kr>
2020-11-22 10:54:38 +0900

    about 가변인자

--- function.py
+++ function.py
@@ -25,6 +25,7 @@ print(balance)
 commission, balance = withdraw_night(balance, 500)
 print("수수료 {0} 원이며, 잔액은 {1} 원입니다." .format(commission, balance))
 
+
 #기본값
 def profile(name, age=17, main_lang="파이썬"):
     print("이름: {0}\t나이 : {1}\t주 사용 언어: {2}" \
@@ -32,4 +33,19 @@ def profile(name, age=17, main_lang="파이썬"):
 
 profile("유재석",20,"파이썬")
 profile("김태호")
+#키워드값
 profile(main_lang="자바", age=25, name="김태호")
+
+#가변인자
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름: {0}\t나이: {1}\t" .format(name,age), end= " ") #end를 이렇게 붙혀주면, 아래 print문과 하나의 문장으로 출력!
+#     print(lang1, lang2, lang3, lang4, lang5)
+
+def profile(name,age,*language): #매개변수 앞에 * 붙히면 가변인자로 사용
+    print("이름: {0}\t나이: {1}\t" .format(name,age), end= " ") #end를 이렇게 붙혀주면, 아래 print문과 하나의 문장으로 출력!
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+profile("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+profile("김태호",25,"Kotlin","Swift")
