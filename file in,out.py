d2b71d29f9645e0e9d5f117d08977b30f6d8c977
JB <shin0343@mail.hongik.ac.kr>
2021-01-04 22:11:58 +0900

    About file input,output

---
+++ file in,out.py
@@ -0,0 +1,26 @@
+# score_file = open("score.txt","w",encoding="utf8")
+# print("수학 : 0", file=score_file)
+# print("영어 : 50",file=score_file)
+# score_file.close()
+
+score_file = open("score.txt", "a", encoding="utf8")
+score_file.write("과학 : 80")
+score_file.write("\n영어 : 60")
+score_file.write("\n수학 : 40")
+score_file.write("\n코딩 : 100")
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline()) # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동까지 함
+while True:
+    line = score_file.readline()
+    if not line:
+        break
+    else:
+        print(line)
+score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() # list 형태로 저장
+for line in lines:
+    print(line, end="")
